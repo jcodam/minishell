362af663fe8d6fee9a6da9d16c7a2f5e63a948f5
jcodam <71557417+jcodam@users.noreply.github.com>
2023-04-26 15:01:24 +0200

    Wed Apr 26 15:01:24 CEST 2023

--- executor/and_or_loop.c
+++ executor/and_or_loop.c
@@ -6,7 +6,7 @@
 /*   By: jbax <jbax@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/12 14:17:03 by jbax          #+#    #+#                 */
-/*   Updated: 2023/04/19 17:54:05 by jbax          ########   odam.nl         */
+/*   Updated: 2023/04/26 14:58:51 by jbax          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,6 @@
 
 int	what_cmd2(t_tokens *bigdata, t_super *super);
 
-// int	what(t_tokens *bigdata, t_super *super)
-// {
-// 	what_cmd2(bigdata, super);
-// 	bigdata = bigdata->next;
-// 	while (bigdata)
-// 	{
-// 		if (g_exit_code && bigdata->log_op == OPP_OR)
-// 		{
-// 			what_cmd2(bigdata, super);
-// 		}
-// 		bigdata = bigdata->next;
-// 	}
-// 	return (1);
-// }
-
-	// // ft_putarrs_fd(bigdata->args, 1);
-	// bigdata->args = arr_expander(bigdata->args, super->env, 0);
-	// // ft_putarrs_fd(bigdata->files, 1);
-	// bigdata->files = arr_expander(bigdata->files, super->env, 1);
 int	what_cmd(t_tokens *bigdata, t_super *super)
 {
 	what_cmd2(bigdata, super);
@@ -45,6 +26,5 @@ int	what_cmd(t_tokens *bigdata, t_super *super)
 			what_cmd2(bigdata, super);
 		bigdata = bigdata->next;
 	}
-	(void)super;
 	return (1);
 }
